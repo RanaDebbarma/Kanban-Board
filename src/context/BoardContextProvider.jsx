a70772b1a38f3d0a839840bc6b0a3557c1017764
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-02-25 07:22:41 +0530

    separate reducer function

--- src/context/BoardContextProvider.jsx
+++ src/context/BoardContextProvider.jsx
@@ -1,23 +1,14 @@
 import { BoardContext } from "./BoardContext";
 import { useReducer } from "react";
+import boardReducer from "../reducers/boardReducer";
 
 const initialState = {
-    input: [],
-    wip: [],
-    output: [],
     column: [],
     cards: [],
 };
 
-function reducer(state, action) {
-    switch(action.type) {
-        case 'ADD_COLUMN':
-            return state;
-    }
-}
-
 export default function BoardContextProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(boardReducer, initialState);
 
   return (
     <BoardContext.Provider value={{ state, dispatch }}>
---
+++ src/reducers/boardReducer.jsx
@@ -0,0 +1,8 @@
+export default function boardReducer(state, action) {
+  switch (action.type) {
+    case "ADD_COLUMN":
+      return state;
+    default:
+      return state;
+  }
+}
