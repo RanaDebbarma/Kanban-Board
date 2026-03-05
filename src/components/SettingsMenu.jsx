269c686421a4254f62b17d45959652952af47a6c
RanaDebbarma <ranadebbarma2001@gmail.com>
2026-03-05 18:39:13 +0530

    themeState integrated

--- src/components/SettingsMenu.jsx
+++ src/components/SettingsMenu.jsx
@@ -1,12 +1,39 @@
+import { useEffect } from "react";
+import useTheme from "../hooks/useTheme";
 import styles from "./Settings.module.css";
 
 export default function SettingsMenu({ showMenu }) {
+  const { themeState } = useTheme();
+
+  useEffect(() => {
+    const theme = themeState.themes[themeState.activeTheme];
+
+    Object.entries(theme).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(`--${key}`, value);
+    });
+
+  }, [themeState]);
+
   return (
     <div
       className={`${styles.settingsMenu} ${showMenu && styles.showSettingsMenu}`}
     >
-      Settings
+      <div className={styles.heading}>
+        Settings
+      </div>
       <hr />
+      <div className={styles.compartment}>
+        compartment
+      </div>
+      <hr />
+      <div className={styles.footer}>
+        <button className={styles.resetBtn}>
+          reset
+        </button>
+        <button className={styles.saveBtn}>
+          save
+        </button>
+      </div>
     </div>
   );
 }
--- src/context/ThemeContextProvider.jsx
+++ src/context/ThemeContextProvider.jsx
@@ -3,30 +3,36 @@ import { ThemeContext } from "./ThemeContext";
 import themeReducer from "../reducers/themeReducer";
 
 const initialState = {
-  bgMain: "#0e0f12",
-  bgColumn: "hsl(220, 17%, 8%)",
-  bgCard: "hsl(224, 14%, 12%)",
-  textPrimary: "hsl(197, 18%, 92%)",
-  textSecondary: "#8596ab",
-  accent: "#00ffbb",
-  border: "hsl(223, 20%, 21%)",
-  shadow: "0 4px 12px rgba(0,0,0,0.4)",
-  bgStage: "hsl(220, 17%, 2%)",
-  bgMenu: "rgba(0, 0, 0, 0.5)",
-  blur: "blur(5px)",
-  input: "hsl(20, 80%, 55%)",
-  wip: "hsl(210, 100%, 55%)",
-  output: "hsl(140, 80%, 55%)",
-  glow: "hsl(164, 100%, 50%, 0.6)",
-  alert: "hsl(0, 100%, 60%)",
-  scrollbarColor: "var(--bg-card)",
+  activeTheme: ["default"],
+  
+  themes: {
+    default: {
+      "bg-main": "#0e0f12",
+      "bg-column": "hsl(220, 17%, 8%)",
+      "bg-card": "hsl(224, 14%, 12%)",
+      "text-primary": "hsl(197, 18%, 92%)",
+      "text-secondary": "#8596ab",
+      "accent": "#00ffbb",
+      "border": "hsl(223, 20%, 21%)",
+      "shadow": "0 4px 12px rgba(0,0,0,0.4)",
+      "bg-stage": "hsl(220, 17%, 2%)",
+      "bg-menu": "rgba(0, 0, 0, 0.5)",
+      "blur": "blur(5px)",
+      "input": "hsl(20, 80%, 55%)",
+      "wip": "hsl(210, 100%, 55%)",
+      "output": "hsl(140, 80%, 55%)",
+      "glow": "hsl(164, 100%, 50%, 0.6)",
+      "alert": "hsl(0, 100%, 60%)",
+      "scrollbar-color": "var(--bg-card)",
+    },
+  },
 };
 
 export default function ThemeContextProvider({ children }) {
-  const [state, dispatch] = useReducer(themeReducer, initialState);
+  const [themeState, dispatchTheme] = useReducer(themeReducer, initialState);
 
   return (
-    <ThemeContext.Provider value={{ state, dispatch }}>
+    <ThemeContext.Provider value={{ themeState, dispatchTheme }}>
       {children}
     </ThemeContext.Provider>
   );
